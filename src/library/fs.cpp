6ab638f7ca6748482d923b2f8b26ecf9af91117f
iambrj <joshibharathiramana@gmail.com>
2019-03-08 15:19:07 +0530

    implemented format

--- src/library/fs.cpp
+++ src/library/fs.cpp
@@ -21,18 +21,18 @@ void FileSystem::debug(Disk *disk) {
 	if(block.Super.MagicNumber == 0xf0f03410) 
 	{
 		std::cout << "magic number is valid" << std::endl;
+		std::cout << "\t" << block.Super.Blocks << " blocks" << std::endl;
+		std::cout << "\t" << block.Super.InodeBlocks << " inode blocks" << std::endl;
+		std::cout << "\t" << block.Super.Inodes << " inodes" << std::endl;
 	}
 	else 
 	{
 		std::cout << "magic number is invalid" << std::endl;
 	}
-	std::cout << "\t" << block.Super.Blocks << " blocks" << std::endl;
-	std::cout << "\t" << block.Super.InodeBlocks << " inode blocks" << std::endl;
-	std::cout << "\t" << block.Super.Inodes << " inodes" << std::endl;
 
 	// Read Inode blocks
 
-	for(uint32_t i = 0; i < INODES_PER_BLOCK; i++)
+	for(uint32_t i = 1; i <= FileSystem::INODES_PER_BLOCK; i++)
 	{
 		if(block.Inodes[i].Valid)
 		{
@@ -46,9 +46,57 @@ void FileSystem::debug(Disk *disk) {
 // Format file system ----------------------------------------------------------
 
 bool FileSystem::format(Disk *disk) {
+
+	std::cout << "Beginning format..." << std::endl;
+
 	// Write superblock
 
+	Block block;
+	std::cout << "Creating SuperBlock..." << std::endl;
+
+	std::cout << "Setting MagicNumber to 0xf0f03410" << std::endl; 
+	block.Super.MagicNumber = 0xf0f03410;
+	std::cout << "MagicNumber set" << std::endl;
+
+	std::cout << "Setting Blocks to " << disk -> size() << std::endl;
+	block.Super.Blocks = disk -> size();
+	std::cout << "Blocks set to " << disk -> size() << std::endl;
+
+
+	std::cout << "Setting InodeBlocks (" << INODES_PERCENT << "%) to " << disk -> size() / FileSystem::INODES_PERCENT << std::endl;
+	block.Super.InodeBlocks = disk -> size() / FileSystem::INODES_PERCENT;
+	std::cout << "InodeBlocks set to " << block.Super.InodeBlocks << std::endl;
+
+
+	std::cout << "Setting Inodes to " << block.Super.InodeBlocks * FileSystem::INODES_PER_BLOCK << std::endl;
+	block.Super.Inodes = block.Super.InodeBlocks * FileSystem::INODES_PER_BLOCK;
+	std::cout << "Set Inodes to " << block.Super.InodeBlocks * FileSystem::INODES_PER_BLOCK << std::endl;
+
+	std::cout << "Writing superblock to disk..." << std::endl;
+	disk -> Disk::write(0, &block.Data);
+	std::cout << "SuperBlock written..." << std::endl;
+
+	std::cout << "Writing inode table..." << std::endl;
+	for(uint32_t i = 1; i <= block.Super.InodeBlocks; i++)
+	{
+		Block iblock;
+		for(uint32_t j = 0; j < FileSystem::INODES_PER_BLOCK; j++)
+		{
+			iblock.Inodes[j].Valid = 0;
+		}
+		disk -> Disk::write(i, &iblock);
+	}
+	std::cout << "Inode table written" << std::endl;
+
 	// Clear all other blocks
+
+	std::cout << "Clearing remaining blocks..." << std::endl;
+	for(size_t i = 1 + block.Super.InodeBlocks; i < disk -> size(); i++)
+	{
+		char temp = 0;
+		disk -> Disk::write(i, &temp);
+	}
+	std::cout << "Remaining blocks cleared" << std::endl;
 	return true;
 }
 
